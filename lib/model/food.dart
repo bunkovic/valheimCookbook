828384fcedc0e8a9a90a8ebb2278af7718f7d14d
piotr <ptrkaz@gmail.com>
2021-03-14 21:30:38 +0100

    create initial data models

--- lib/model/food.dart
+++ lib/model/food.dart
@@ -1,16 +1,18 @@
-class Food {
+import 'package:valheim_cookbook/model/ingredient.dart';
+
+class Food implements Craftable {
+  final int id;
   final String name;
   final int health;
   final int hpPerTick;
   final int stamina;
   final int duration;
+  @override
   final int stackSize;
+  @override final List<Ingredient> ingredients;
+  @override
+  final double weight;
 
-  Food(
-      {this.name,
-      this.health,
-      this.hpPerTick,
-      this.stamina,
-      this.duration,
-      this.stackSize});
+  Food(this.weight, {this.id,this.name, this.health, this.hpPerTick, this.stamina, this.duration, this.stackSize, this.ingredients});
+
 }
--- lib/model/ingredient.dart
+++ lib/model/ingredient.dart
@@ -1,7 +1,15 @@
 class Ingredient{
   final int quantity;
-  final CraftingMaterial material;
+  final Item item;
+  Ingredient(this.quantity, this.item);
 }
-class CraftingMaterial{
+abstract class Item{
+  //TODO that might get resolved when
+  final int id = -1;
+  final double weight = 0.0;
+  final int stackSize = 1;
+}
 
+abstract class Craftable implements Item{
+  final List<Ingredient> ingredients = List.empty();
 }
